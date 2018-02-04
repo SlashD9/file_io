7ebd366b3c0516b9c704a62a1dab4fea4f9756ad
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-04 09:19:42 +0000

    added the game loop function to allow us to capture the options selected from the menu, from here we can do something based on what option is given by the user

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,5 +5,18 @@ def show_menu():
     
     option = input("Enter option: ")
     return option
-    
-print(show_menu())
+
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You slected 'Ask questions'")
+        elif option == "2":
+            print("You selected 'Add a question")
+        elif option == "3":
+            break
+        else:
+            print("Invalid Option")
+        print("")
+            
+game_loop()
