08aec70fdc9caa97fe4047abf1dadc4413e4d838
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-04 10:58:49 +0000

    Tuple to join both the questions and answers together, then we ask the question and put the answer into a guess variable

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -18,7 +18,29 @@ def add_question():
     file.write(question + "\n")
     file.write(answer + "\n")
     file.close()
+
+def ask_question():
+    # first we will create two lists to hold our quesitions and answers
+    questions = []
+    answers = []
     
+    # the with block helps with the file handling, we don't need to worry about 
+    # closing the file.
+    with open("questions.txt", "r") as file:
+        lines = file.read().splitlines()
+    
+    # for i and text in the list of lines
+    for i, text in enumerate(lines):
+        # if the line is even, add that line to questions
+        if i%2 == 0:
+            questions.append(text)
+        # else add the line to answers
+        else:
+            answers.append(text)
+    
+    # Create a Tuple of questions and answers.
+    for question, answer in zip(questions, answers):
+        guess = input(question + "> ")
 
 def game_loop():
     while True:
